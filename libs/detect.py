4dbf08016837d41e2791f0189584545a65bc5410
Hüseyin Çayırlı <62603427+hcayirli97@users.noreply.github.com>
2022-10-18 17:13:03 +0300

    Update detect.py

--- libs/detect.py
+++ libs/detect.py
@@ -6,8 +6,6 @@ from models.common import DetectMultiBackend
 from utils.general import non_max_suppression, scale_boxes
 from utils.dataloaders import letterbox
 
-names = ['10c', '10d', '10h', '10s', '2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s', '4c', '4d', '4h', '4s', '5c', '5d', '5h', '5s', '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s', '8c', '8d', '8h', '8s', '9c', '9d', '9h', '9s', 'Ac', 'Ad', 'Ah', 'As', 'Jc', 'Jd', 'Jh', 'Js', 'Kc', 'Kd', 'Kh', 'Ks', 'Qc', 'Qd', 'Qh', 'Qs']
-
 def detect(im ,model, device,imgsz):
     global names
     Frame = im.copy()
